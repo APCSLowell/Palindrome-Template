4e8baa8e212b65b4b9a8ac052c69a9a5c41da806
mrchanr <42151767+mrchanr@users.noreply.github.com>
2026-01-07 20:04:40 +0000

    seperate tests

--- lib/src/test/java/org/APCSLowell/PalindromeTest.java
+++ lib/src/test/java/org/APCSLowell/PalindromeTest.java
@@ -12,13 +12,33 @@ import static org.junit.Assert.*;
 
 public class PalindromeTest {
     @Test
-    public void wordTest() {
+    public void testTest() {
         Palindrome p = new Palindrome();
         assert(!p.palindrome("test"));
+    }
+    @Test
+    public void testRotator() {
+        Palindrome p = new Palindrome();
         assert(p.palindrome("rotator"));
+    }
+    @Test
+    public void testRewriter() {
+        Palindrome p = new Palindrome();
         assert(!p.palindrome("rewriter"));
+    }
+    @Test
+    public void testNusesRun() {
+        Palindrome p = new Palindrome();
         assert(p.palindrome("nurses run"));
+    }
+    @Test
+    public void testMadamImAdam() {
+        Palindrome p = new Palindrome();
         assert(p.palindrome("Madam, I'm Adam"));
+    }
+    @Test
+    public void testAManAPlanACanalPanama() {
+        Palindrome p = new Palindrome();
         assert(p.palindrome("A Man! A Plan! A Canal! Panama!"));
     }
 }
